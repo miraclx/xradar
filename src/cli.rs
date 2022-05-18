104bc7be2801d8cd2a70ad315a3dd830d6f6efc9
Miraculous Owonubi <omiraculous@gmail.com>
2022-05-18 06:14:48 +0100

    extract cli-specific logic into cli module

---
+++ src/cli.rs
@@ -0,0 +1,77 @@
+use std::num::{NonZeroU16, NonZeroUsize};
+use std::str::FromStr;
+
+use clap::{ArgEnum, Parser};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+pub struct Args {
+    /// The host to connect to. (defaults to "localhost")
+    pub host: Option<String>,
+    /// An optional, non-zero port number to check. (defaults to scanning all ports)
+    ///
+    /// Format: `80`, `8000..8888`, `666,777,888,999`.
+    #[clap(
+        multiple_occurrences = true,
+        use_value_delimiter = true,
+        default_value = "-"
+    )]
+    pub port: Vec<CliPort>,
+    /// Port test method to use.
+    #[clap(arg_enum, short = 'm', default_value_t = Method::Fast)]
+    pub method: Method,
+    /// Show status of all ports, open or closed. (defaults to false)
+    #[clap(short = 'a')]
+    pub all: bool,
+    /// The number of threads to use. (defaults to the number of CPUs)
+    #[clap(short = 'n')]
+    pub threads: Option<NonZeroUsize>,
+    /// Be verbose. Query information about each port. (defaults to false)
+    #[clap(short = 'v')]
+    pub verbose: bool,
+}
+
+#[derive(ArgEnum, Eq, Clone, Debug, PartialEq)]
+pub enum Method {
+    #[clap(alias("f"))]
+    Fast,
+    #[clap(alias("s"))]
+    Slow,
+}
+
+#[derive(Debug)]
+pub struct CliPort(pub Vec<super::Port>);
+
+impl FromStr for CliPort {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // support ranges like "80..8888", "80-8888" or "-" and ".." for all ports
+        let ports = if let Some((l, r)) = s.split_once("..").or_else(|| s.split_once("-")) {
+            let l = if l.is_empty() {
+                1
+            } else {
+                l.parse::<NonZeroU16>().map_err(|e| e.to_string())?.get()
+            };
+
+            let r = if r.is_empty() {
+                u16::max_value()
+            } else {
+                r.parse::<NonZeroU16>().map_err(|e| e.to_string())?.get()
+            };
+
+            (l..=r)
+                .map(|port| super::Port {
+                    num: port,
+                    open: false,
+                })
+                .collect::<Vec<_>>()
+        } else {
+            vec![super::Port {
+                num: s.parse::<NonZeroU16>().map_err(|e| e.to_string())?.get(),
+                open: false,
+            }]
+        };
+        Ok(Self(ports))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,10 @@
 use std::collections::HashSet;
-use std::num::{NonZeroU16, NonZeroUsize};
-use std::str::FromStr;
 
-use clap::{ArgEnum, Parser};
+use clap::Parser;
 use futures::stream::StreamExt;
 
+mod cli;
 mod methods;
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about)]
-struct CLIArgs {
-    /// The host to connect to. (defaults to "localhost")
-    host: Option<String>,
-    /// An optional, non-zero port number to check. (defaults to scanning all ports)
-    ///
-    /// Format: `80`, `8000..8888`, `666,777,888,999`.
-    #[clap(
-        multiple_occurrences = true,
-        use_value_delimiter = true,
-        default_value = "-"
-    )]
-    port: Vec<CliPort>,
-    /// Port test method to use.
-    #[clap(arg_enum, short = 'm', default_value_t = Method::Fast)]
-    method: Method,
-    /// Show status of all ports, open or closed. (defaults to false)
-    #[clap(short = 'a')]
-    all: bool,
-    /// The number of threads to use. (defaults to the number of CPUs)
-    #[clap(short = 'n')]
-    threads: Option<NonZeroUsize>,
-    /// Be verbose. Query information about each port. (defaults to false)
-    #[clap(short = 'v')]
-    verbose: bool,
-}
-
-#[derive(ArgEnum, Eq, Clone, Debug, PartialEq)]
-enum Method {
-    #[clap(alias("f"))]
-    Fast,
-    #[clap(alias("s"))]
-    Slow,
-}
 
 #[derive(Eq, Ord, Hash, Debug, PartialEq, PartialOrd)]
 pub struct Port {
@@ -49,53 +12,16 @@ pub struct Port {
     open: bool,
 }
 
-#[derive(Debug)]
-struct CliPort(Vec<Port>);
-
-impl FromStr for CliPort {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // support ranges like "80..8888", "80-8888" or "-" and ".." for all ports
-        let ports = if let Some((l, r)) = s.split_once("..").or_else(|| s.split_once("-")) {
-            let l = if l.is_empty() {
-                1
-            } else {
-                l.parse::<NonZeroU16>().map_err(|e| e.to_string())?.get()
-            };
-
-            let r = if r.is_empty() {
-                u16::max_value()
-            } else {
-                r.parse::<NonZeroU16>().map_err(|e| e.to_string())?.get()
-            };
-
-            (l..=r)
-                .map(|port| Port {
-                    num: port,
-                    open: false,
-                })
-                .collect::<Vec<_>>()
-        } else {
-            vec![Port {
-                num: s.parse::<NonZeroU16>().map_err(|e| e.to_string())?.get(),
-                open: false,
-            }]
-        };
-        Ok(Self(ports))
-    }
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = CLIArgs::parse();
+    let args = cli::Args::parse();
 
     let host = args.host.unwrap_or("localhost".to_string());
 
     let mut ports = args
         .port
         .into_iter()
-        .map(|CliPort(p)| p)
+        .map(|p| p.0)
         .flatten()
         .collect::<Vec<_>>();
 
@@ -111,11 +37,11 @@ async fn main() -> anyhow::Result<()> {
     println!("Threads: {}", threads);
 
     let mut ports = match args.method {
-        Method::Slow => {
+        cli::Method::Slow => {
             println!("Method : Slow");
             methods::client::run(host, ports, threads).await
         }
-        Method::Fast => {
+        cli::Method::Fast => {
             println!("Method : Fast");
             todo!()
             // methods::server::run(args.host, ports, threads);
